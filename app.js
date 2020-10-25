af79b6976dae41c7303af8c5a7e71d93bd5ab545
Samuel Skanberg <samuel.skanberg@gmail.com>
2020-10-25 15:57:55 +0100

    Show notification when timer is done

--- app.js
+++ app.js
@@ -1,6 +1,46 @@
 $(document).ready(function() {
     console.log("Page loaded");   
 
+    // Make sure we get permissions for notifications before running the timer :)
+    // We only ask if we haven't already asked
+    if ("Notification" in window) {
+        if (Notification.permission === "default") {
+            Notification.requestPermission().then(function(p) {
+                if(p === 'granted') {
+                    var notification = new Notification("Notifications activated");
+                } else {
+                    console.log('User blocked notifications.');
+                }
+            }).catch(function(err) {
+                console.error(err);
+            });
+        }
+    }
+
+    function notifyUser(message) {
+      // Let's check if the browser supports notifications
+      if (!("Notification" in window)) {
+	console.log("This browser does not support desktop notification");
+      }
+
+      // Let's check whether notification permissions have already been granted
+      else if (Notification.permission === "granted") {
+	// If it's okay let's create a notification
+	var notification = new Notification(message);
+      }
+
+      // Otherwise, we need to ask the user for permission
+      // This should not be needed since we have already asked in the beginning
+      else if (Notification.permission !== "denied") {
+	Notification.requestPermission().then(function (permission) {
+	  // If the user accepts, let's create a notification
+	  if (permission === "granted") {
+	    var notification = new Notification(message);
+	  }
+	});
+      }
+    }
+
     var timer;
     var limit;
     var count;
@@ -18,11 +58,13 @@ $(document).ready(function() {
     function long_timer_done() {
         console.log("Long timer done");
         $("div#timer-info").html("Long timer is done"); 
+        notifyUser("Long timer is done");
     }
 
     function short_timer_done() {
         console.log("Short timer done");
         $("div#timer-info").html("Short timer is done"); 
+        notifyUser("Short timer is done");
     }
 
     function start_timer() {
@@ -33,23 +75,26 @@ $(document).ready(function() {
         }, 1000);
     }
 
-    $('#long-timer-button').click(function() {
+    $('#long-timer-button').click(function(event) {
+        event.preventDefault();
         console.log("Clicked long timer");
         $("div#timer-info").html("Long timer is running...");
-        limit = 25*60;
+        limit = 10;
         timer_done = long_timer_done;
         start_timer();
     });
 
-    $('#short-timer-button').click(function() {
+    $('#short-timer-button').click(function(event) {
+        event.preventDefault();
         console.log("Clicked short timer");
         $("div#timer-info").html("Short timer is running...");
-        limit = 5*60;
+        limit = 5;
         timer_done = short_timer_done;
         start_timer();
     });
 
-    $('#stop-timer-button').click(function() {
+    $('#stop-timer-button').click(function(event) {
+        event.preventDefault();
         console.log("Clicked stop timer");
         $("div#timer-info").html("Timer is stopped"); 
         clearInterval(timer);
